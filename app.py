c61a03c5b0a22f99daa74d8649fd4a1bad5be18f
Mexiale Grace <gracemexiale@yahoo.fr>
2021-08-04 22:38:15 +0000

    New fonction

--- app.py
+++ app.py
@@ -4,18 +4,34 @@ Author : Mexiale
 """
 
 import numpy as np
-from flask import Flask, request, jsonify, render_template
+import pandas as pd
+from flask import Flask, request, jsonify, render_template, g
 import pickle
+import sqlite3 as sql
 
 app = Flask(__name__)
 model = pickle.load(open('model.pkl', 'rb'))
+DATABASE = 'Db.db'
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sql.connect(DATABASE)
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
 
 @app.route('/')
 def home():
+    cur = get_db().cursor()
     return render_template('index.html')
 
 @app.route('/predict',methods=['POST'])
-def predict():
+def Predict():
     '''
     For rendering results on HTML GUI
     '''
@@ -42,6 +58,37 @@ def predict_api():
     output = prediction[0]
     return jsonify(output)
 
+
+@app.route('/search', methods=['POST', 'GET'])
+def list():
+    nb= 1
+    if request.form.get('nombre'):
+        nb = request.form['nombre']
+    con = sql.connect("Db.sqlite3")
+    con.row_factory = sql.Row
+   
+    cur = con.cursor()
+    cur.execute(f"select Gender, Customer_Age, Total_Relationship_Count, Months_Inactive_12_mon, Total_Revolving_Bal, Total_Trans_Amt, Avg_Utilization_Ratio, Attrition_Flag from Client_Bank ORDER BY RANDOM() LIMIT {nb}")
+    rows = cur.fetchall();
+    datas = np.array(rows)
+    datas = pd.DataFrame(datas, columns=['Gender', 'Customer_Age', 'Total_Relationship_Count', 'Months_Inactive_12_mon', 'Total_Revolving_Bal', 'Total_Trans_Amt', 'Avg_Utilization_Ratio', 'Attrition_Flag'])
+    datas['Gender'].replace({'M': 1, 'F': 0}, inplace=True)
+    features = datas.drop(['Attrition_Flag'], axis=1)
+    prediction = model.predict(features)
+    ypred = prediction.reshape(int(nb), 1)
+    ypred = pd.DataFrame(ypred, columns=['Prediction'])
+    datas = pd.concat([datas, ypred], axis=1)
+    datas['Gender'].replace({1: 'M', 0: 'F'}, inplace=True)
+    datas['Prediction'].replace({0: 'Existing Customer', 1: 'Attrited Customer'}, inplace=True)
+    return render_template("Predic_BD.html",datas = datas.values.tolist())
+
+@app.route('/Predic_form')
+def Predic_form():
+    return render_template("Predic_form.html")
+
+@app.route('/Predic_BD')
+def Predic_BD():
+    return render_template("Predic_BD.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
